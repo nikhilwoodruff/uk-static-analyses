c5f9f901172a92d1c2d9e43542ed7fa93c69fdd0
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2020-09-27 02:49:48 +0100

    Update CSV generation

--- csv_generation/generate_csv.py
+++ csv_generation/generate_csv.py
@@ -11,8 +11,17 @@ def clean_dirs(output_dir):
         shutil.rmtree(output_dir)
     os.makedirs(output_dir)
 
+def safe(*texts):
+    num = 0
+    for text in texts:
+        try:
+            num += float(text)
+        except:
+            pass
+    return num
 
-def write_person_file(data_dir, output_dir, weights=None):
+
+def write_person_file(data_dir, output_dir):
     with open(
         os.path.join(output_dir, "person.csv"),
         "w+",
@@ -26,23 +35,31 @@ def write_person_file(data_dir, output_dir, weights=None):
             fieldnames = [
                 "person_id",
                 "household_id",
-                "family_id",
+                "benunit_id",
                 "role",
                 "is_male",
+                "is_head",
                 "is_state_pension_age",
                 "age_band",
-                "JSA_receipt",
-                "IS_receipt",
-                "private_pension_actual",
-                "employee_earnings",
-                "self_employed_earnings",
-                "state_pension_actual",
-                "investment_income",
                 "hours_worked",
                 "adult_weight",
-                "disabled",
+                "disabled"
             ]
-            writer = csv.DictWriter(g, fieldnames=fieldnames)
+            same_name_fields = [
+                "INEARNS",
+                "SEINCAM2",
+                "ININV",
+                "INTXCRED",
+                "INDISBEN",
+                "INOTHBEN",
+                "INRPINC",
+                "INPENINC",
+                "INRINC",
+                "INDUC",
+                "INDINC",
+                "NINDINC"
+            ]
+            writer = csv.DictWriter(g, fieldnames=fieldnames + same_name_fields)
             writer.writeheader()
             skipped = 0
             person_data = {}
@@ -50,12 +67,6 @@ def write_person_file(data_dir, output_dir, weights=None):
                 reader, desc="Writing adults into person file", total=33238
             ):
                 try:
-                    string_keys = [
-                        "person_id",
-                        "household_id",
-                        "family_id",
-                        "role",
-                    ]
                     person_id = (
                         line["sernum"]
                         + "f"
@@ -63,38 +74,23 @@ def write_person_file(data_dir, output_dir, weights=None):
                         + "p"
                         + line["PERSON"]
                     )
-                    allowed_to_be_negative = ["self_employed_earnings"]
                     person = {
                         "person_id": line["sernum"] + "p" + line["PERSON"],
                         "household_id": line["sernum"],
-                        "family_id": line["sernum"] + "f" + line["BENUNIT"],
+                        "benunit_id": line["sernum"] + "f" + line["BENUNIT"],
                         "is_male": line["SEX"] == 1,
+                        "is_head": line["COMBID"] == 1,
                         "is_state_pension_age": line["PENFLAG"] == "1",
                         "role": "adult",
                         "age_band": int(line["IAGEGR4"]),
-                        "JSA_receipt": 1 if line["WAGEBEN6"] == "1" else 0,
-                        "IS_receipt": 1 if line["WAGEBEN5"] == "1" else 0,
-                        "private_pension_actual": line["INPENINC"],
-                        "employee_earnings": line["INEARNS"],
-                        "self_employed_earnings": line["INCSEO2"]
-                        if line["INCSEO2"] != " "
-                        else 0,
-                        "investment_income": line["ININV"],
-                        "hours_worked": line["TOTHOURS"]
-                        if line["TOTHOURS"] != " "
-                        else 0,
+                        "hours_worked": safe(line["TOTHOURS"]),
                         "adult_weight": line["GROSS4"],
-                        "disabled": line["LAREG"] == "1",
-                        "state_pension_actual": 0,
+                        "disabled": line["DISACTA1"] == "1"
                     }
-                    for key in person.keys():
-                        if (
-                            key not in string_keys
-                            and key not in allowed_to_be_negative
-                        ):
-                            assert float(person[key]) >= 0
+                    for field in same_name_fields:
+                        person[field] = safe(line[field])
                     person_data[person_id] = person
-                except Exception as e:
+                except:
                     skipped += 1
             print(f"Adults: skipped {skipped} rows.")
 
@@ -107,12 +103,6 @@ def write_person_file(data_dir, output_dir, weights=None):
                 reader, desc="Writing children into person file", total=9849
             ):
                 try:
-                    string_keys = [
-                        "person_id",
-                        "household_id",
-                        "family_id",
-                        "role",
-                    ]
                     person_id = (
                         line["sernum"]
                         + "f"
@@ -123,23 +113,17 @@ def write_person_file(data_dir, output_dir, weights=None):
                     person = {
                         "person_id": line["sernum"] + "p" + line["PERSON"],
                         "household_id": line["sernum"],
-                        "family_id": line["sernum"] + "f" + line["BENUNIT"],
+                        "benunit_id": line["sernum"] + "f" + line["BENUNIT"],
                         "role": "child",
                         "age_band": 0,
-                        "JSA_receipt": 0,
-                        "IS_receipt": 0,
-                        "private_pension_actual": 0,
-                        "employee_earnings": 0,
-                        "self_employed_earnings": 0,
-                        "investment_income": 0,
-                        "state_pension_actual": 0,
                         "hours_worked": 0,
                         "adult_weight": 0,
-                        "disabled": line["LAREG"] == "1",
+                        "disabled": line["DISACTC1"] == "1"
                     }
-                    for key in person.keys():
-                        if key not in string_keys:
-                            assert float(person[key]) >= 0
+                    for field in same_name_fields:
+                        person[field] = 0
+                    person["INRINC"] = safe(line["CHRINC"])
+                    person["INEARNS"] = safe(line["CHEARNS"])
                     person_data[person_id] = person
                 except:
                     skipped += 1
@@ -153,7 +137,6 @@ def write_person_file(data_dir, output_dir, weights=None):
             )
             skipped = 0
             benefit_codes = {
-                "5": "state_pension",
             }
             for line in tqdm(
                 reader, desc="Writing benefits into person file", total=38475
@@ -181,9 +164,9 @@ def write_person_file(data_dir, output_dir, weights=None):
         print("Wrote person file.")
 
 
-def write_family_file(data_dir, output_dir, weights=None):
+def write_benunit_file(data_dir, output_dir, weights=None):
     with open(
-        os.path.join(output_dir, "family.csv"),
+        os.path.join(output_dir, "benunit.csv"),
         "w+",
         encoding="utf-8",
         newline="",
@@ -196,57 +179,67 @@ def write_family_file(data_dir, output_dir, weights=None):
             )
             fieldnames = [
                 "household_id",
-                "family_id",
-                "JSA_actual",
-                "num_children_actual",
-                "income_support_actual",
-                "housing_benefit_actual",
-                "child_tax_credit_actual",
-                "working_tax_credit_actual",
-                "child_benefit_actual",
-                "family_weight",
+                "benunit_id",
+                "benunit_weight",
             ]
-            writer = csv.DictWriter(g, fieldnames=fieldnames)
-            writer.writeheader()
             skipped = 0
-            family_data = {}
+            benunit_data = {}
+            benefit_codes = {
+                "1": "DLA_SC",
+                "2": "DLA_M",
+                "3": "child_benefit",
+                "4": "pension_credit",
+                "5": "state_pension",
+                "6": "BSP",
+                "8": "AFCS",
+                "10": "SDA",
+                "12": "AA",
+                "13": "carers_allowance",
+                "15": "IIDB",
+                "16": "ESA",
+                "17": "incapacity_benefit",
+                "19": "income_support",
+                "21": "maternity_allowance",
+                "37": "guardians_allowance",
+                "62": "winter_fuel_payments",
+                "90": "working_tax_credit",
+                "91": "child_tax_credit",
+                "94": "housing_benefit",
+                "95": "universal_credit",
+                "96": "PIP_DL",
+                "97": "PIP_M"
+            }
+            benefit_names = list(map(lambda benefit : benefit + "_reported", benefit_codes.values()))
+            contr = "JSA_contributory"
+            inc = "JSA_income"
+            comb = "JSA_combined"
+            JSA_names = list(map(lambda x : x + "_reported", [contr, inc, comb]))
+            JSA_types = {
+                "1": contr,
+                "2": inc,
+                "3": contr,
+                "4": inc,
+                "5": comb,
+                "6": comb
+            }
+            writer = csv.DictWriter(g, fieldnames=fieldnames + benefit_names + JSA_names)
+            writer.writeheader()
             for line in tqdm(
-                reader, desc="Writing families into family file", total=22406
+                reader, desc="Writing benunits into benunit file", total=22406
             ):
                 try:
-                    string_keys = [
-                        "household_id",
-                        "family_id",
-                        "family_structure",
-                    ]
-                    family_id = line["sernum"] + "f" + line["BENUNIT"]
-                    try:
-                        total_benefit_income = (
-                            float(line["BUIRBEN"])
-                            + float(line["BUNIRBEN"])
-                            + float(line["BUOTHBEN"])
-                        )
-                    except:
-                        total_benefit_income = 0
-                    family = {
+                    benunit_id = line["sernum"] + "f" + line["BENUNIT"]
+                    benunit = {
                         "household_id": line["sernum"],
-                        "family_id": family_id,
-                        "num_children_actual": line["DEPCHLDB"],
-                        "JSA_actual": 0,
-                        "child_benefit_actual": 0,
-                        "income_support_actual": 0,
-                        "housing_benefit_actual": 0,
-                        "child_tax_credit_actual": 0,
-                        "working_tax_credit_actual": 0,
-                        "family_weight": line["GROSS4"],
+                        "benunit_id": benunit_id,
+                        "benunit_weight": line["GROSS4"],
                     }
-                    for key in family.keys():
-                        if key not in string_keys:
-                            assert float(family[key]) >= 0
-                    family_data[family_id] = family
-                except Exception as e:
+                    for field in benefit_names + JSA_names:
+                        benunit[field] = 0
+                    benunit_data[benunit_id] = benunit
+                except:
                     skipped += 1
-            print(f"Families: skipped {skipped} rows.")
+            print(f"Benunits: skipped {skipped} rows.")
 
         with open(
             os.path.join(data_dir, "benefits.tab"), encoding="utf-8"
@@ -255,32 +248,27 @@ def write_family_file(data_dir, output_dir, weights=None):
                 f, fieldnames=next(f).split("\t"), delimiter="\t"
             )
             skipped = 0
-            benefit_codes = {
-                "14": "JSA",
-                "3": "child_benefit",
-                "19": "income_support",
-                "94": "housing_benefit",
-                "91": "child_tax_credit",
-                "90": "working_tax_credit",
-            }
             for line in tqdm(
-                reader, desc="Writing benefits into family file", total=38475
+                reader, desc="Writing benefits into benunit file", total=38475
             ):
                 try:
-                    family_id = line["sernum"] + "f" + line["BENUNIT"]
-                    if line["BENEFIT"] in benefit_codes:
-                        family_data[family_id][
-                            benefit_codes[line["BENEFIT"]] + "_actual"
-                        ] = float(line["BENAMT"])
+                    benunit_id = line["sernum"] + "f" + line["BENUNIT"]
+                    code = line["BENEFIT"]
+                    if code in benefit_codes:
+                        benunit_data[benunit_id][
+                            benefit_codes[code] + "_reported"
+                        ] = safe(line["BENAMT"])
+                    elif code == "14":
+                        benunit_data[benunit_id][JSA_types[line["VAR2"]] + "_reported"] = safe(line["BENAMT"])
                 except:
                     skipped += 1
             print(f"Benefits: skipped {skipped} rows.")
 
-        for family_id, family in tqdm(
-            family_data.items(), desc="Writing family file"
+        for benunit_id, benunit in tqdm(
+            benunit_data.items(), desc="Writing benunit file"
         ):
-            writer.writerow(family)
-        print("Wrote family file.")
+            writer.writerow(benunit)
+        print("Wrote benunit file.")
 
 
 def write_household_file(data_dir, output_dir, weights=None):
@@ -296,7 +284,7 @@ def write_household_file(data_dir, output_dir, weights=None):
             reader = csv.DictReader(
                 f, fieldnames=next(f).split("\t"), delimiter="\t"
             )
-            fieldnames = ["household_id", "household_weight"]
+            fieldnames = ["household_id", "household_weight", "housing_cost", "council_tax"]
             writer = csv.DictWriter(g, fieldnames=fieldnames)
             writer.writeheader()
             skipped = 0
@@ -310,12 +298,14 @@ def write_household_file(data_dir, output_dir, weights=None):
                     household = {
                         "household_id": line["sernum"],
                         "household_weight": line["GROSS4"],
+                        "housing_cost": safe(line["GBHSCOST"]),
+                        "council_tax": safe(line["CTANNUAL"]) / 52
                     }
                     for key in household.keys():
                         if key not in string_keys:
                             assert float(household[key]) >= 0
                     writer.writerow(household)
-                except Exception as e:
+                except:
                     skipped += 1
             print(f"Households: skipped {skipped} rows.")
 
@@ -337,5 +327,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     clean_dirs(args.output_dir)
     write_person_file(args.data_dir, args.output_dir)
-    write_family_file(args.data_dir, args.output_dir)
+    write_benunit_file(args.data_dir, args.output_dir)
     write_household_file(args.data_dir, args.output_dir)
