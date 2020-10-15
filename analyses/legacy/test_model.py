c36608b74b05f6740a3122f2f686b5d62dbd1fb4
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2020-10-15 16:47:17 +0100

    Remove old files

--- analyses/legacy/test_model.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from openfisca_uk import CountryTaxBenefitSystem
-from openfisca_core.simulation_builder import SimulationBuilder
-from openfisca_uk.reforms.basic_income import bi_from_pa
-from utilities.simulation import model
-from utilities.visualisation import banded_lineplot
-import numpy as np
-import seaborn as sns
-from matplotlib import pyplot as plt
